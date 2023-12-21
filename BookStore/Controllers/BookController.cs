12d8ecd0f40bbc95ddb45ce6a7cd9152649f1598
IvanTerziyski <stu2013891002@uni-plovdiv.bg>
2023-12-21 18:20:58 +0200

    Author Addition

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -1,4 +1,6 @@
-﻿using BookStore.DL.Interfaces;
+﻿using BookStore.BL.Interfaces;
+using BookStore.BL.Services;
+using BookStore.DL.Interfaces;
 using BookStore.DL.Repositories;
 using BookStore.Models.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -9,18 +11,41 @@ namespace BookStore.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
-        private readonly IBookRepository _bookRepository;
-        public BookController(IBookRepository bookRepository)
+        private readonly IBookService _bookService;
+        public BookController(IBookService bookService)
         {
-            _bookRepository = bookRepository;
+            _bookService = bookService;
         }
 
-        [HttpGet]
-        public Book? Get(int id)
+        [HttpGet("GetById")]
+        public Book? GetBook(int id)
         {
-
-            if (id < 0) return null;
-            return _bookRepository.GetBook(id);
+            return _bookService.GetBook(id);
         }
+
+        [HttpPost("Add")]
+        public void AddBook([FromBody] Book book)
+        {
+            _bookService.AddBook(book);
+        }
+
+        [HttpDelete("Delete")]
+        public void DeleteBook(int id) 
+        { 
+            _bookService.DeleteBook(id);
+        }
+
+        [HttpPost("Update")]
+        public void UpdateBook([FromBody] Book book)
+        {
+            _bookService.UpdateBook(book);
+        }
+
+        [HttpGet("GetAllBooks")]
+        public List<Book> GetAllBooks()
+        {
+            return _bookService.GetAllBooks();
+        }
+        
     }
 }
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,5 +1,9 @@
 using BookStore.DL.Interfaces;
 using BookStore.DL.Repositories;
+using BookStore.BL.Interfaces;
+using BookStore.BL.Services;
+
+
 
 namespace BookStore
 {
@@ -10,12 +14,15 @@ namespace BookStore
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddSingleton<IBookRepository, BookRepository>();
+            builder.Services.AddSingleton<IBookService, BookService>();
+            builder.Services.AddSingleton<IAuthorRepository, AuthorRepository>();
+            builder.Services.AddSingleton<IAuthorService, AuthorService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<IBookRepository, BookRepository>();
 
             var app = builder.Build();
 
