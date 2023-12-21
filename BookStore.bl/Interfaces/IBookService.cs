57356bd766e7b5bd109ea0870ac361ab054084e4
IvanTerziyski <stu2013891002@uni-plovdiv.bg>
2023-12-21 18:13:03 +0200

    Author Addition

---
+++ BookStore.bl/Interfaces/IBookService.cs
@@ -0,0 +1,19 @@
+﻿using BookStore.Models.Models;
+
+namespace BookStore.BL.Interfaces
+{
+    public interface IBookService
+    {
+        public void AddBook(Book book);
+
+        public void DeleteBook(int id);
+
+        public void UpdateBook(Book book);
+
+
+        public Book? GetBook(int id);
+
+
+        public List<Book> GetAllBooks();
+    }
+}
---
+++ BookStore.bl/Services/AuthorService.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.BL.Interfaces;
+using BookStore.DL.Interfaces;
+using BookStore.Models.Models;
+
+namespace BookStore.BL.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly IAuthorRepository _AuthorRepository;
+        public AuthorService(IAuthorRepository AuthorRepository)
+        {
+            _AuthorRepository = AuthorRepository;
+        }
+        public void AddAuthor(Author Author)
+        {
+            _AuthorRepository.AddAuthor(Author);
+        }
+
+        public void DeleteAuthor(int id)
+        {
+            _AuthorRepository.DeleteAuthor(id);
+        }
+
+        public List<Author> GetAllAuthors()
+        {
+            return _AuthorRepository.GetAllAuthors();
+        }
+
+        public Author? GetAuthor(int id)
+        {
+            return _AuthorRepository.GetAuthor(id);
+        }
+
+        public void UpdateAuthor(Author Author)
+        {
+            _AuthorRepository.UpdateAuthor(Author);
+        }
+    }
+}
